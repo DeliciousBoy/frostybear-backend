cc6f6c044e1247e4152a2594d9e99190a0c51fc0
Rawee sinbamrung (Trainee) <Rawee_pond30@hotmail.com>
2025-03-08 01:02:47 +0700

    Connect:(Database):FrostyBear

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -6,18 +6,4 @@ const router = express.Router()
 
 router.get('/products', productC.getAllProduct)
 
-router.get('/products/ten', productC.getTenProduct)
-
-router.get('/products/:id', productC.getProductById)
-
-router.get('/products/search/:id', productC.getSearchProduct)
-
-router.post('/products', productC.postProduct)
-
-router.put('/products/:id', productC.putProduct)
-
-router.delete('/products/:id', productC.deleteProduct)
-
-router.get('/products/brands/:id',productC.getProductByBrandId)
-
 export default router
