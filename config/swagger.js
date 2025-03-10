5ccac050c71c4f20aece45a63336332a50d95bdd
Rawee sinbamrung (Trainee) <Rawee_pond30@hotmail.com>
2025-03-10 19:20:05 +0700

    เพิ่ม Schema ทุกๆ table ใน db

--- config/swagger.js
+++ config/swagger.js
@@ -7,6 +7,61 @@ const doc = {
   },
   host: "localhost:3000",
   schemes: ["http"],
+  definitions: {
+    cart_details: {
+      type: "object",
+      properties: {
+        quantity: { type: "number" },
+        price: { type: "number" },
+        cart_id: { type: "string" },
+        product_id: { type: "string" },
+      },
+    },
+    carts: {
+      type: "object",
+      properties: {
+        card_date: { type: "string", format: "date" },
+        cart_id: { type: "string" },
+        customer_id: { type: "string" },
+      },
+    },
+    product_types: {
+      type: "object",
+      properties: {
+        product_type_id: { type: "string" },
+        product_type_name: { type: "string" },
+      },
+    },
+    users: {
+      type: "object",
+      properties: {
+        created_at: { type: "string", format: "date-time" },
+        id: { type: "string" },
+        username: { type: "string" },
+        password: { type: "string" },
+        role: { type: "string" },
+      },
+    },
+    products: {
+      type: "object",
+      properties: {
+        product_price: { type: "number" },
+        product_name: { type: "string" },
+        product_image: { type: "string" },
+        product_id: { type: "string" },
+        brand_id: { type: "string" },
+        product_type: { type: "string" },
+        product_detail: { type: "string" },
+      },
+    },
+    brands: {
+      type: "object",
+      properties: {
+        brand_id: { type: "string" },
+        brand_name: { type: "string" },
+      },
+    },
+  },
 };
 
 const outputFile = "./service/swagger_output.json"; // กำหนด path ของ output
